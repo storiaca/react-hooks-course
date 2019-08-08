fc37a6e84d21a2b6a5fa53c459779cf594e0f50f
Aleksandar Ristic <storiaca@gmail.com>
2019-08-08 13:36:12 +0200

    useEffect example with inputs

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { useForm } from "./useForm";
 function App() {
-  const [values, handleChange] = useForm({ email: "", password: "" });
+  const [values, handleChange] = useForm({
+    email: "",
+    password: "",
+    firstName: ""
+  });
+  useEffect(() => {
+    console.log("render");
+  }, [values.email, values.password]);
   return (
     <div>
       <form>
         <input
+          name="firstName"
+          type="text"
+          placeholder="First Name"
+          value={values.firstName}
+          onChange={handleChange}
+        />
+        <input
           name="email"
           type="text"
+          placeholder="Email"
           value={values.email}
           onChange={handleChange}
         />
         <input
           name="password"
           type="password"
+          placeholder="Password"
           value={values.password}
           onChange={handleChange}
         />
