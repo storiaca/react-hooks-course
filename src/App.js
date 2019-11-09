fc28255355eab1ea63fd997010f092dafc6fd01f
storiaca <storiaca@gmail.com>
2019-11-09 13:14:22 +0100

    Create custom hook useFetch

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "./useForm";
 //import Hello from "./Hello";
+import { useFetch } from "./useFetch";
+
 function App() {
-  //const [showHello, setShowHello] = useState(true);
   const [values, handleChange] = useForm({
     email: "",
     password: "",
     firstName: ""
   });
-  // useEffect(() => {
-  //   const onMouseMove = e => {
-  //     console.log(e);
-  //   };
-  //   window.addEventListener("mousemove", onMouseMove);
-  //   return () => {
-  //     window.removeEventListener("mousemove", onMouseMove);
-  //   };
-  // }, []);
-  useEffect(() => {
-    console.log("mount1");
-  }, []);
-  useEffect(() => {
-    console.log("mount2");
-  }, []);
+
+  const { data, loading } = useFetch("http://numbersapi.com/43/trivia");
+  //http://numbersapi.com/43/trivia
 
   return (
     <div>
