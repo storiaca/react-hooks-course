1a058af6896d227506fdcc381f33382a89d1c4e5
storiaca <storiaca@gmail.com>
2019-11-10 14:37:18 +0100

    Save data to localStorage with useEffect

--- src/App.js
+++ src/App.js
@@ -10,8 +10,14 @@ function App() {
     firstName: ""
   });
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() =>
+    JSON.parse(localStorage.getItem("count"))
+  );
   const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
 
   return (
     <div>
